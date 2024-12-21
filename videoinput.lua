a1ddcdd1a4fac6fb69e6f661ca819e33af023740
mikethe223 <117932754+mikethe223@users.noreply.github.com>
2024-12-21 16:08:54 +0100

    missing =

--- videoinput.lua
+++ videoinput.lua
@@ -5,6 +5,6 @@ return function(nodemgr)
     videoInputNode.id="digital-video-input"
     videoInputNode.about="Node that returns a digital video file inputted by the user."
     videoInputNode.outputs={nodemgr.ioElem.digital}
-    videoInputNode.color{0.49019607843137253,0.8549019607843137,0.34509803921568627}
+    videoInputNode.color={0.49019607843137253,0.8549019607843137,0.34509803921568627}
     return videoInputNode
 end
