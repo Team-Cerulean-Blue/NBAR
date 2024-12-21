75a31d90999d52115fe7aeeae8540e9f024b3a6c
mikethe223 <117932754+mikethe223@users.noreply.github.com>
2024-12-21 16:07:38 +0100

    Update nodes.lua
    test to see if custom color works. if DVI node is green, congr me.

--- nodes.lua
+++ nodes.lua
@@ -92,7 +92,7 @@ end,
 
     love.graphics.setColor(0.7,0.7,0.7)
     love.graphics.rectangle("line",rectBox[1],rectBox[2],rectBox[3],rectBox[4],5,5,5)
-    love.graphics.setColor(0.21,0.21,0.30)
+    love.graphics.setColor(scrnode.node.color,0.2)
     love.graphics.rectangle("fill",rectBox[1],rectBox[2],rectBox[3],rectBox[4],5,5,5)
 
     love.graphics.setColor(1.0,1.0,1.0)
