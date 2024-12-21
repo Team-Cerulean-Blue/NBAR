3c822824fd268cf70dd52ec40f1e0985b3647be1
mikethe223 <117932754+mikethe223@users.noreply.github.com>
2024-12-21 13:37:08 +0100

    lessrounding and general visual fixes

--- nodes.lua
+++ nodes.lua
@@ -89,10 +89,10 @@ end,
         renderIO(input,ioX,ioY)
     end
 
-    love.graphics.setColor(0,0,0)
-    love.graphics.rectangle("line",rectBox[1],rectBox[2],rectBox[3],rectBox[4],10,10,5)
-    love.graphics.setColor(1,1,1)
-    love.graphics.rectangle("fill",rectBox[1],rectBox[2],rectBox[3],rectBox[4],10,10,5)
+    love.graphics.setColor(0.2,0.2,0.2)
+    love.graphics.rectangle("line",rectBox[1],rectBox[2],rectBox[3],rectBox[4],5,5,5)
+    love.graphics.setColor(1,0.5,1)
+    love.graphics.rectangle("fill",rectBox[1],rectBox[2],rectBox[3],rectBox[4],5,5,5)
 
     love.graphics.setColor(0,0,0)
     love.graphics.print(wrappedText,rectBox[1]+nodeRenderPadding,rectBox[2]+nodeRenderPadding)
