b8014e957cd00229c5815550823e133d8d4bb55e
mikethe223 <117932754+mikethe223@users.noreply.github.com>
2024-12-21 13:23:16 +0100

    Update video input title

--- videoinput.lua
+++ videoinput.lua
@@ -1,6 +1,6 @@
 return function(nodemgr)
     local videoInputNode = table.shallow_copy(nodemgr.nodeClass)
-    videoInputNode.title="Digital video input The FitnessGram Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues"
+    videoInputNode.title="Digital video input"
     videoInputNode.id="digital-video-input"
     videoInputNode.about="Node that returns a digital video file inputted by the user."
     videoInputNode.outputs={nodemgr.ioElem.digital}
